8b5b1dffc6b714ec83a3be86f75238d31300eac4
MJHR <majohr89@gmail.com>
2018-10-13 12:35:15 +0200

    item route created

---
+++ routes/item.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+const uploadCloud = require('../configs/cloudinary.js');
+
+var Item = require('../models/item');
+
+router.get('/', (req, res, next) => {
+  Item.find()
+    .then((results) => {
+      res.status(200).json(results);
+    })
+    .catch(next);
+});
+
+router.post('/', uploadCloud.single('file'), (req, res, next) => {
+  const item = new Item({
+    name: req.body.name,
+    image: req.file.url
+  });
+
+  item.save((err) => {
+    if (err) return res.json(err);
+    return res.json({
+      message: 'New clothing added',
+      item: item
+    });
+  });
+});
+
+module.exports = router;
