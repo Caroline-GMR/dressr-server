3c26f696d201943e30a3db8e84ef6530f46accf8
MJHR <majohr89@gmail.com>
2018-10-14 16:59:16 +0200

    checkbox tests

--- routes/item.js
+++ routes/item.js
@@ -13,40 +13,51 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
-router.post('/', uploadCloud.single('file'), (req, res, next) => {
-  console.log('que tengo en body', req.body);
-  const item = new Item({
-    picture: req.file.url,
-    category: req.body.category,
-    description: req.body.description,
-    style: req.body.style
+// router.post('/', uploadCloud.single('file'), (req, res, next) => {
+//   console.log('que tengo en body', req.body);
+//   const item = new Item({
+//     picture: req.file.url,
+//     category: req.body.category,
+//     description: req.body.description,
+//     style: req.body.style
 
-  });
-  console.log('file', req.file.url);
-  item.save((err) => {
-    if (err) return res.json(err);
-    return res.json({
-      message: 'New clothing added',
-      item: item
-    });
-  });
-});
-
-// router.post('/', uploadCloud.single('picture'), (req, res, next) => {
-//   const owner = req.session.currentUser._id;
-//   let picture;
-//   let { category, description, style } = req.body;
-//   if (req.file) {
-//     picture = req.file.url;
-//     console.log('img', req.file.url);
-//   }
-//   const item = new Item({ category, description, style, owner, picture });
-//   item.save()
-//     .then(() => {
-//       res.redirect(`/item/${item._id}`);
-//     })
-//     .catch(next);
+//   });
+//   console.log('file', req.file.url);
+//   item.save((err) => {
+//     if (err) return res.json(err);
+//     return res.json({
+//       message: 'New clothing added',
+//       item: item
+//     });
+//   });
 // });
+
+router.post('/', uploadCloud.single('picture'), (req, res, next) => {
+  // const input = req.body.input;
+  // if (input === 'dress' || 't-shirt' || 'jacket' || 'hoodie' || 'blouse' || 'jersey') {
+  //   this.input = this.category.top;
+  // }
+  // if (input === 'jeans' || 'skirt' || 'shorts' || 'leggings' || 'pants' || 'sweatpants') {
+  //   this.input = this.category.bottom;
+  // }
+  // if (input === 'sneakers' || 'boots' || 'loafers' || 'highheels' || 'ballerinas' || 'sandals') {
+  //   this.input = this.category.footwear;
+  // };
+
+  const owner = req.session.currentUser._id;
+  let picture;
+  let { category, description, style } = req.body;
+  if (req.file) {
+    picture = req.file.url;
+    console.log('img', req.file.url);
+  }
+  const item = new Item({ category, description, style, owner, picture });
+  item.save()
+    .then(() => {
+      res.redirect(`/item/${item._id}`);
+    })
+    .catch(next);
+});
 
 router.get('/_id', (req, res, next) => {
   const id = req.params.id;
