341b98cca5faa8e9e0f990ae91abf6d3ae82ffa2
MJHR <majohr89@gmail.com>
2018-10-17 13:02:59 +0200

    add missing api error middlewares

--- app.js
+++ app.js
@@ -50,4 +50,19 @@ app.use('/auth', authRouter);
 app.use('/item', itemRouter);
 app.use('/item/:id', itemRouter);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ code: 'unexpected' });
+  }
+});
+
 module.exports = app;
